ae0f7b7a22ed3ec29464b1cce95e9a083952e90a
qa-dmma <78306016+qa-dmma@users.noreply.github.com>
2023-07-25 00:23:23 +0300

    Innitial Commit

---
+++ Lesson_16/src/Main/java/com/hillel/ProductMarket/foodShelf/ProductMarket.java
@@ -0,0 +1,10 @@
+package com.hillel.ProductMarket.foodShelf;
+
+public class ProductMarket {
+
+    public ProductMarket(String name, int price) {
+        StorageProduct.ArrayProduct(name, price);
+    }
+
+
+}
