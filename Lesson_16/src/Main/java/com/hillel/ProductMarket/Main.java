d77ad7860c24fc663771b689cd208ef5f8575f58
qa-dmma <78306016+qa-dmma@users.noreply.github.com>
2023-07-29 18:36:40 +0300

    Optimized code, removed unnecessary classes and constructs

--- Lesson_16/src/Main/java/com/hillel/ProductMarket/Main.java
+++ Lesson_16/src/Main/java/com/hillel/ProductMarket/Main.java
@@ -1,20 +1,25 @@
 package com.hillel.ProductMarket;
 
+import com.hillel.ProductMarket.foodShelf.Product;
 import com.hillel.ProductMarket.foodShelf.ProductMarket;
-import com.hillel.ProductMarket.foodShelf.StorageProduct;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ProductMarket product0 = new ProductMarket("orange", 15);
-        ProductMarket product1 = new ProductMarket("apple", 7);
-        ProductMarket product2 = new ProductMarket("cucumber", 2);
-        ProductMarket product3 = new ProductMarket("banana", 11);
-        ProductMarket product4 = new ProductMarket("strawberry", 4);
-        ProductMarket product5 = new ProductMarket("coconut", 19);
-        StorageProduct.getProductNames();
-        StorageProduct.getSortedProductNames();
-        StorageProduct.getPriceMoreThen(10, '>');
-        StorageProduct.getPriceLessThen(5, '<');
-        StorageProduct.getPriceString();
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("orange", 15));
+        productList.add(new Product("apple", 7));
+        productList.add(new Product("cucumber", 2));
+        productList.add(new Product("banana", 11));
+        productList.add(new Product("strawberry", 4));
+        productList.add(new Product("coconut", 19));
+        ProductMarket productMarket = new ProductMarket("Shelf", productList);
+        System.out.println(productMarket.returnProductName());
+        System.out.println(productMarket.sortProductName());
+        System.out.println(productMarket.sortByHightPrice());
+        System.out.println(productMarket.sortByLowerPrice());
+        System.out.println(productMarket.priceToString());
     }
 }
